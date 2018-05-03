2ae46b7d1ab6ee50f679f3fa9cb8de4154d4cf81
spikir <spikirard@gmail.com>
2018-05-03 13:47:46 +0200

    Added bootstrap and changed structure
    Added bootstrap and changed structure

--- src/index.js
+++ src/index.js
@@ -3,6 +3,57 @@ import { render } from "react-dom";
 
 let noteId = 0;
 
+const ToDoForm = ({ addToDo, state, changeToDo }) => {
+  return (
+    <div className="row">
+      <div className="col-md-10 col-sm-10">
+        <input
+          className="form-control"
+          type="text"
+          value={state}
+          onChange={changeToDo}
+        />
+      </div>
+      <div className="col-md-2 col-sm-2">
+        <button
+          type="button"
+          className="btn btn-default btn-xl btn-block"
+          id="addButton"
+          onClick={() => {
+            addToDo();
+          }}
+        >
+          Add note
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const ToDo = ({ todo, keyId, remove }) => {
+  return (
+    <li className="list-group-item clearfix">
+      {todo}
+      <button
+        type="button"
+        className="btn pull-right"
+        onClick={() => {
+          remove(keyId);
+        }}
+      >
+        Delete note
+      </button>
+    </li>
+  );
+};
+
+const ToDoList = ({ todos, remove }) => {
+  const todoNote = todos.map(c => {
+    return <ToDo todo={c.note} keyId={c.id} remove={remove} />;
+  });
+  return <ul className="list-group">{todoNote}</ul>;
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -14,35 +65,19 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="lists">
-        <h1>{this.props.name}</h1>
-        <div className="form">
-          <input
-            className="inputNote"
-            type="text"
-            value={this.state.inputNote}
-            onChange={this.handleChange.bind(this)}
+      <div className="container-fluid">
+        <h1 className="text-center">{this.props.name}</h1>
+
+        <ToDoForm
+          addToDo={this.addNote.bind(this)}
+          stateToDo={this.state.inputNote}
+          changeToDo={this.handleChange.bind(this)}
+        />
+        <div className="row">
+          <ToDoList
+            todos={this.state.notes}
+            remove={this.deleteNote.bind(this)}
           />
-          <button className="addButton" onClick={this.addNote.bind(this)}>
-            Add note
-          </button>
-          <ul>
-            <h2>
-              {this.state.notes.map(c => {
-                return (
-                  <li>
-                    {c.note}{" "}
-                    <button
-                      className="deleteButton"
-                      onClick={this.deleteNote.bind(this, c.id)}
-                    >
-                      Delete note
-                    </button>
-                  </li>
-                );
-              })}
-            </h2>
-          </ul>
         </div>
       </div>
     );
